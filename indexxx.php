2b66e7a19b317567967938a7a37bf5fc626fd0aa
frederikanovianti <128951421+frederikanovianti@users.noreply.github.com>
2023-03-26 01:08:44 -0700

    Add files via upload

---
+++ indexxx.php
@@ -0,0 +1,132 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <link rel="stylesheet" href="style.css" >
+    <title>BOX MOVIES</title>
+   
+</head>
+<body>
+<?php
+        if(isset($_POST['cari'])){
+            $judul = $_POST['judul'];
+            echo "<h1>Hasil Pencarian</h1>";
+        
+            $url = 'http://www.omdbapi.com/?apikey=202bed7d&s="'.$judul.'"';
+
+           //Akses API dengan CURL
+            $ch = curl_init();
+            curl_setopt($ch, CURLOPT_URL, $url);
+            curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+            $output = curl_exec($ch);
+            curl_close($ch);
+
+            // var_dump($output);
+            $data = json_decode($output, TRUE);
+            // $data = $data['Search'];
+            
+            foreach ($data['Search'] as $movie) {
+                
+                echo "\n <p>Judul: ".$movie["Title"]."</p>";
+                echo "<p>Tahun: ".$movie["Year"]."</p>";
+                echo '<img src= "'.$movie['Poster'].'">';
+            }    
+        }
+
+        ?> 
+        
+  <header>
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+
+  <h3>BOX MOVIES</h3>
+    <form method="post" action="index.php">
+        <input type="text" name="judul"></input>
+        <input type="submit" value="pencarian" name="cari"></input>
+     
+    </form>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav ms-0">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="#">Home</a>
+        </li>  
+      </ul>
+    </div>
+  </div>
+</nav>
+</header>
+<div class="container">
+<div class="row mt-3 text-center">
+  <div class="col">
+<h1>HMM</h1>
+</div>
+</div>
+
+    
+<div class="row row-cols-1 row-cols-md-3 g-4">
+  <div class="col">
+    <div class="card">
+      <img src="brian.jpg" class="card-img-top" height="400px">
+      <div class="card-body">
+        <h5 class="card-title">Card title</h5>
+        <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+      </div>
+    </div>
+  </div>
+  <div class="col">
+    <div class="card">
+      <img src="brian122.jpg" class="card-img-top" height="400px">
+      <div class="card-body">
+        <h5 class="card-title">Card title</h5>
+        <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+      </div>
+    </div>
+  </div>
+  <div class="col">
+    <div class="card">
+      <img src="88.PNG" class="card-img-top" height="400px">
+      <div class="card-body">
+        <h5 class="card-title">Card title</h5>
+        <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content.</p>
+      </div>
+    </div>
+  </div>
+  <div class="col">
+    <div class="card">
+      <img src="brian5.jpg" class="card-img-top" height="400px">
+      <div class="card-body">
+        <h5 class="card-title">Card title</h5>
+        <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content.</p>
+      </div>
+    </div>
+  </div>
+  <div class="col">
+    <div class="card">
+      <img src="111.PNG" class="card-img-top" height="400px">
+      <div class="card-body">
+        <h5 class="card-title">Card title</h5>
+        <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content.</p>
+      </div>
+    </div>
+  </div>
+  <div class="col">
+    <div class="card">
+      <img src="brian10.jpg" class="card-img-top" height="400px">
+      <div class="card-body">
+        <h5 class="card-title">Card title</h5>
+        <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content.</p>
+      </div>
+    </div>
+  </div>
+  
+    </div>
+   
+   
+  
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+
+</body>
+</html>
